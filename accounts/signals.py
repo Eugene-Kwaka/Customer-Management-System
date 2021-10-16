ef616e6c0438e92fceda21c810226ae9a4a1525f
Eugene Kwaka <eugenekwaka@gmail.com>
2021-10-16 23:53:47 +0300

    Added .env file

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,7 +6,7 @@ from .models import Customer
 
 def customer_profile(sender, instance, created, **kwargs):
     if created:
-        group = Group.objects.get(name='customer')
+        group, created = Group.objects.get_or_create(name='customer')
         instance.groups.add(group)
 
         Customer.objects.create(
--- cmw/settings.py
+++ cmw/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4(jq6ao%4%l1sxag=(*5w2#nk51g!qg7a8t70(tns72p#@_f(o'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'cmw.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CMW',
-        'USER': 'postgres',
-        'PASSWORD': 'GwayanKwaks',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME':  config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
     }
 }
 
@@ -138,9 +139,9 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # SMTP Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'babukwaks.bk@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
