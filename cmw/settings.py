cc25326c8a63890dabee05d6122f2fd86bc698b7
Eugene Kirima <kwakaeugene@gmail.com>
2022-01-19 22:26:06 +0300

    part 3

--- cmw/settings.py
+++ cmw/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,13 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
