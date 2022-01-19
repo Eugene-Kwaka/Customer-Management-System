33e40461d5aa83c9a445b33829e1b31d25f03e27
Eugene Kirima <kwakaeugene@gmail.com>
2022-01-19 21:04:22 +0300

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cmw.wsgi --log-file - 
--- cmw/settings.py
+++ cmw/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eugenecmw.herokuapp.com', '127.0.0.1' ]
 
 
 # Application definition
