cf26c3c0ee835780029ae7ffd131905907b68c19
Eugene Kirima <kwakaeugene@gmail.com>
2022-01-21 18:47:20 +0300

    part 10

--- cmw/settings.py
+++ cmw/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
