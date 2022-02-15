dfe9b2b3b8034ff04d4bf84e11c850fadeb3286a
Eugene Kirima <kwakaeugene@gmail.com>
2022-02-15 12:28:36 +0300

    changes

--- cmw/settings.py
+++ cmw/settings.py
@@ -174,20 +174,20 @@ SITE_ID = 1
 AWS_ACCESS_KEY_ID=config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL=None #This works to ensure that the images to be publicly viewable
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400'
-}
-AWS_LOCATION = 'static'
-AWS_QUERYSTRING_AUTH = False
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin'
-}
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400'
+# }
+# AWS_LOCATION = 'static'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_HEADERS = {
+#     'Access-Control-Allow-Origin'
+# }
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE= 'storages.backends.s3boto3.S3BotoStorage'
-STATIC_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/static/' 
-MEDIA_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' 
+# STATIC_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/static/' 
+# MEDIA_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' 
 
 # AWS_S3_FILE_OVERWRITE=config('AWS_S3_FILE_OVERWRITE')
 # AWS_DEFAULT_ACL=config('AWS_DEFAULT_ACL')
