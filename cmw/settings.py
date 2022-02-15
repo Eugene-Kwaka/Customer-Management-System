308b3ec7b8fb2f33eea3115f9099e5dcbed1f95b
Eugene Kirima <kwakaeugene@gmail.com>
2022-02-15 12:33:57 +0300

    changes

--- cmw/settings.py
+++ cmw/settings.py
@@ -144,12 +144,10 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-# MEDIA_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -175,7 +173,7 @@ AWS_ACCESS_KEY_ID=config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')
 #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL=None #This works to ensure that the images to be publicly viewable
+AWS_DEFAULT_ACL= None #This works to ensure that the images to be publicly viewable
 # AWS_S3_OBJECT_PARAMETERS = {
 #     'CacheControl': 'max-age=86400'
 # }
